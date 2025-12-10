e942417f5e3797cc001b83a4d9fd8c0f5f57eba3
Aditya Sharma <adityasharma2005123@gmail.com>
2025-12-10 22:47:07 +0530

    Added example for Next.js implementation

--- src/App.tsx
+++ src/App.tsx
@@ -57,3 +57,45 @@ function App() {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+// Code for a Next.js Client app - 
+
+
+
+
+// "use client"
+// import { useEffect, useState } from 'react'
+
+// export default function() {
+//   const [socket, setSocket] = useState<WebSocket | null>(null);
+
+//   useEffect(() => {
+//     const newSocket = new WebSocket('ws://localhost:8080');
+//     newSocket.onopen = () => {
+//       console.log('Connection established');
+//       newSocket.send('Hello Server!');
+//     }
+//     newSocket.onmessage = (message) => {
+//       console.log('Message received:', message.data);
+//     }
+//     setSocket(newSocket);
+//     return () => newSocket.close();
+//   }, [])
+
+//   return (
+//     <>
+//       hi there
+//     </>
+//   )
+// }
